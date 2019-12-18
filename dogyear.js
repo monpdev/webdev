30107505ecdb4af701b2fac2a53bfd7b328fea44
monpdev <35469743+monpdev@users.noreply.github.com>
2019-12-18 01:52:34 +0000

    Add files via upload
    Javascript file to convert human years to dog years. Part of Web Dev Track/Codecademy.

---
+++ dogyear.js
@@ -0,0 +1,25 @@
+//Create variable my age
+const myAge = 20;
+
+//Define first 2 years of a dog's life
+let earlyYears = 2;
+
+//Multiply earlyYears by 10.5
+earlyYears *= 10.5;
+
+//laterYears equals myAge subtracted by 2
+let laterYears = myAge -2;
+
+//Calculate laterYears in dog years
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'MP'.toLowerCase();
+
+console.log(`My name is ${myName}. I am 
+${myAge} years old in human years which is ${myAgeInDogYears} in dog years.`)
+
